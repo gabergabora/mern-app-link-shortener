a9ae24293d74d6cc8149df40d57c13e2cb41d9d0
lConstantine <k.lebedev89@gmil.com>
2020-09-01 12:43:13 +0400

    port; -resolve

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use("/", express.static(path.join(__dirname, 'client', 'build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
-const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || 5000   // config.get('port')
 
 async function start() {
   try {
